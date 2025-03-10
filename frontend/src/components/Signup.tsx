a8eaeebcf2542892a97eae0a027041b4e7846d00
Jeber811 <135171306+Jeber811@users.noreply.github.com>
2025-03-10 19:53:27 -0400

    Update Signup.tsx
    Added firstName, lastName, and email field. Matched coding practices of Login.tsx

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,62 +1,124 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Signup() {
+    const app_name = "777finances.com";
 
-    const [message, setMessage] = useState('');
-    const [signupName, setSignupName] = React.useState('');
-    const [signupPassword, setPassword] = React.useState('');
+    function buildPath(route: string): string {
+        if (process.env.NODE_ENV !== "development") {
+            return "http://" + app_name + ":5000/" + route;
+        } else {
+            return "http://localhost:5000/" + route;
+        }
+    }
+
+    const [message, setMessage] = useState("");
+    const [signupName, setSignupName] = React.useState("");
+    const [signupPassword, setPassword] = React.useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
 
     async function doSignup(event: any): Promise<void> {
         event.preventDefault();
-        var obj = { Signup: signupName, password: signupPassword };
+
+        var obj = {
+            login: signupName, // Matching API field name
+            password: signupPassword,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+        };
+
         var js = JSON.stringify(obj);
+
         try {
-            const response = await fetch('http://localhost:5000/api/signup',
-                {
-                    method: 'POST', body: js, headers: {
-                        'Content-Type':
-                            'application/json'
-                    }
-                });
-            /*
+            const response = await fetch(buildPath("api/signup"), {
+                method: "POST",
+                body: js,
+                headers: { "Content-Type": "application/json" },
+            });
+
             var res = JSON.parse(await response.text());
-            if (res.id <= 0) {
-                setMessage('User/Password combination incorrect');
+
+            if (res.success) {
+                setMessage("Signup successful! Redirecting to login...");
+                setTimeout(() => {
+                    window.location.href = "/login";
+                }, 2000);
+            } else {
+                setMessage(res.error || "Signup failed. Please try again.");
             }
-            else {
-                var user =
-                    { firstName: res.firstName, lastName: res.lastName, id: res.id }
-                localStorage.setItem('user_data', JSON.stringify(user));
-                setMessage('');
-                window.location.href = '/cards';
-            }
-            */
-        }
-        catch (error: any) {
+        } catch (error: any) {
             alert(error.toString());
             return;
         }
-    };
+    }
 
     function handleSetSignupName(e: any): void {
         setSignupName(e.target.value);
     }
-
     function handleSetPassword(e: any): void {
         setPassword(e.target.value);
     }
+    function handleSetFirstName(e: any): void {
+        setFirstName(e.target.value);
+    }
+    function handleSetLastName(e: any): void {
+        setLastName(e.target.value);
+    }
+    function handleSetEmail(e: any): void {
+        setEmail(e.target.value);
+    }
 
     return (
-        <div id="SignupDiv">
-            <span id="inner-title">Please Sign Up</span><br />
-            <input type="text" id="signupName" placeholder="Username" /><br />
-            <input type="password" id="signupPassword" placeholder="Password" /><br />
-            <button type="submit" id="signupButton" className="buttons" onClick={doSignup}>
-                Sign Up
-            </button>
-            <span id="SignupResult"></span>
+        <div id="signupDiv">
+            <span id="inner-title">PLEASE SIGN UP</span>
+            <br />
+            <input
+                type="text"
+                id="firstName"
+                placeholder="First Name"
+                onChange={handleSetFirstName}
+            />
+            <br />
+            <input
+                type="text"
+                id="lastName"
+                placeholder="Last Name"
+                onChange={handleSetLastName}
+            />
+            <br />
+            <input
+                type="email"
+                id="email"
+                placeholder="Email"
+                onChange={handleSetEmail}
+            />
+            <br />
+            <input
+                type="text"
+                id="signupName"
+                placeholder="Username"
+                onChange={handleSetSignupName}
+            />
+            <br />
+            <input
+                type="password"
+                id="signupPassword"
+                placeholder="Password"
+                onChange={handleSetPassword}
+            />
+            <br />
+            <input
+                type="submit"
+                id="signupButton"
+                className="buttons"
+                value="Sign Up"
+                onClick={doSignup}
+            />
+            <span id="signupResult">{message}</span>
         </div>
     );
-};
+}
 
 export default Signup;
