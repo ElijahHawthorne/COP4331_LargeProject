0d67ec42c11049ff7c9b8ebac85fd43d24b702ad
Jeber811 <135171306+Jeber811@users.noreply.github.com>
2025-03-10 16:44:38 -0400

    Added Signup component

--- frontend/src/Pages/LoginPage.tsx
+++ frontend/src/Pages/LoginPage.tsx
@@ -1,11 +1,15 @@
-import PageTitle from "../components/PageTitle.tsx";
-import Login from "../components/Login.tsx";
+import PageTitle from '../components/PageTitle.tsx';
+import Login from '../components/Login.tsx';
+import Signup from '../components/Signup.tsx';
+
 const LoginPage = () => {
-  return (
-    <div>
-      <PageTitle />
-      <Login />
-    </div>
-  );
+    return (
+        <div className="login-content">
+            <PageTitle />
+            <Login />
+            <Signup />
+        </div>
+    );
 };
+
 export default LoginPage;
