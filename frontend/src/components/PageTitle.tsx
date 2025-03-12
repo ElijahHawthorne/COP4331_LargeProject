0c4c147591ccea3e6d957c5a284d77497340f4f0
Elijah Hawthorne <elijah.hawthorne@yahoo.com>
2025-03-12 18:00:07 -0400

    setting up my branch

--- frontend/src/components/PageTitle.tsx
+++ frontend/src/components/PageTitle.tsx
@@ -1,4 +1,4 @@
 function PageTitle() {
-  return <h1 id="title">777 Finances</h1>;
+  return <h1 id="title" className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-black md:text-5xl lg:text-6xl">777Finances</h1>;
 }
 export default PageTitle;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js', // specify the path to your postcss config if needed
-  },
-});
+  plugins: [react(), tailwindcss()],
+})
