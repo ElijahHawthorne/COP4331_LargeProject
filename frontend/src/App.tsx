b72e2629c0dbaf4af847234549076abf8a9fe5b5
nathanr2306 <nathanromand123@gmail.com>
2025-04-05 20:32:21 -0400

    New skeleton. Needs all logic added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
 //import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-import LoginPage from './Pages/LoginPage';
-import CardPage from './Pages/CardPage';
-import SignUpPage from './Pages/SignupPage';
+import Dashboard from './dashboard/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/cards" element={<CardPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
